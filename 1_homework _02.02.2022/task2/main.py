54f24e2add2b623cb94f845d6c4cc289d048c63d
MykytaH <98914407+MykytaH@users.noreply.github.com>
2022-02-04 19:59:25 +0200

    1_homework_02.02.2022
    1_homework_02.02.2022

---
+++ 1_homework _02.02.2022/task2/main.py
@@ -0,0 +1,11 @@
+# 'hello world'
+print('hello world')
+# 'hello world'
+print('hello', 'world')
+# '1_2_3_4'
+print(1, 2, 3, 4, sep='_')
+# 'green line_yellow line'
+# 'red line!'
+print('green line', 'yellow line', sep='-', end='\n')
+print('red line', end='!\n\n\n')
+print("#########\n#\t\t#\n#\t\t#\n#\t\t#\n#########\n\n#\t\t#\n#\t\t#\n#########\n#\t\t#\n#\t\t#")
